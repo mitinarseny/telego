acea975a93d62070b3079c6466d65aecc5ca78da
Arseny Mitin <mitinarseny@gmail.com>
2019-07-31 20:38:30 +0300

    fix: log fatalf

--- cmd/root.go
+++ cmd/root.go
@@ -1,63 +1,63 @@
 package cmd
 
 import (
-    "strings"
+	"strings"
 
-    "github.com/mitinarseny/telego/bot"
-    log "github.com/sirupsen/logrus"
-    "github.com/spf13/cobra"
-    "github.com/spf13/viper"
+	"github.com/mitinarseny/telego/bot"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var rootCmd = &cobra.Command{
-    Run: func(cmd *cobra.Command, args []string) {
-        checkMandatoryParams()
-        start()
-    },
+	Run: func(cmd *cobra.Command, args []string) {
+		checkMandatoryParams()
+		start()
+	},
 }
 
 func start() {
-    if err := bot.Run(viper.GetString("bot.token"),
-        viper.GetString("notifier.bot.token"),
-        viper.GetInt64("notifier.chat.id"),
-        true); err != nil {
-        log.Fatal(err)
-    }
+	if err := bot.Run(viper.GetString("bot.token"),
+		viper.GetString("notifier.bot.token"),
+		viper.GetInt64("notifier.chat.id"),
+		true); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func Execute() {
-    if err := rootCmd.Execute(); err != nil {
-        log.Fatal(err)
-    }
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func init() {
-    cobra.OnInitialize(initConfig)
+	cobra.OnInitialize(initConfig)
 
-    rootCmd.PersistentFlags().String("bot.token", "", "Telegram Bot API token")
-    _ = viper.BindPFlag("bot.token", rootCmd.PersistentFlags().Lookup("bot.token"))
+	rootCmd.PersistentFlags().String("bot.token", "", "Telegram Bot API token")
+	_ = viper.BindPFlag("bot.token", rootCmd.PersistentFlags().Lookup("bot.token"))
 
-    rootCmd.PersistentFlags().String("notifier.bot.token", "", "Notifier Telegram Bot API token")
-    _ = viper.BindPFlag("notifier.bot.token", rootCmd.PersistentFlags().Lookup("notifier.bot.token"))
+	rootCmd.PersistentFlags().String("notifier.bot.token", "", "Notifier Telegram Bot API token")
+	_ = viper.BindPFlag("notifier.bot.token", rootCmd.PersistentFlags().Lookup("notifier.bot.token"))
 
-    rootCmd.PersistentFlags().Int64("notifier.chat.id", -1, "Notifier Chat ID")
-    _ = viper.BindPFlag("notifier.chat.id", rootCmd.PersistentFlags().Lookup("notifier.chat.id"))
+	rootCmd.PersistentFlags().Int64("notifier.chat.id", -1, "Notifier Chat ID")
+	_ = viper.BindPFlag("notifier.chat.id", rootCmd.PersistentFlags().Lookup("notifier.chat.id"))
 }
 
 func initConfig() {
-    viper.SetEnvPrefix("TELEGO")
-    viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-    viper.AutomaticEnv()
+	viper.SetEnvPrefix("TELEGO")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
 }
 
 func checkMandatoryParams() {
-    var missing []string
+	var missing []string
 
-    if v := viper.GetString("bot.token"); v == "" {
-        missing = append(missing, "bot.token")
-    }
+	if v := viper.GetString("bot.token"); v == "" {
+		missing = append(missing, "bot.token")
+	}
 
-    if len(missing) > 0 {
-        log.Fatal("missing: %s", strings.Join(missing, ", "))
-    }
+	if len(missing) > 0 {
+		log.Fatalf("missing: %s", strings.Join(missing, ", "))
+	}
 }
